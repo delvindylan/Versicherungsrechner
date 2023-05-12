c042930927b08e5b57fe97ac3d2ab10cf2a805c4
Delvin <delvin@ngauv.ch>
2023-05-12 12:19:46 +0200

    css done

--- versicherungsrechner/src/components/Calculator.js
+++ versicherungsrechner/src/components/Calculator.js
@@ -6,16 +6,27 @@ export default function Calculator() {
     const [versicherungswert, setVersicherungswert] = useState("")
     const [schadenssumme, setSchadenssumme] = useState("")
     const [schadensdeckung, setSchadensdeckung] = useState("")
+    const [versicherungsStatus, setVersicherungsStatus] = useState("")
+    const [versicherungsText, setVersicherungsText] = useState("")
+
 
     const handleSubmit = (e) => {
         e.preventDefault()
         let calculatedSchadensdeckung = versicherungssumme / versicherungswert * schadenssumme
         let roundedSchadensdeckung = calculatedSchadensdeckung.toFixed(2)
-        setSchadensdeckung(roundedSchadensdeckung)
+        setVersicherungsText("Schadensdeckung der Hausratsversicherung")
+        setSchadensdeckung("CHF " + roundedSchadensdeckung)
         //let currencySchadensdeckung = `CHF ${schadensdeckung}`; noch bearbeiten!!!
         if (versicherungssumme.length < 1 && versicherungswert.length < 1 && schadenssumme.length < 1) {
             alert("Fill in the Values");
-          } 
+            handleClear()
+          }
+
+        if (versicherungssumme > versicherungswert) {
+            setVersicherungsStatus("Sie sind überversichert.")
+        } else if (versicherungswert > versicherungssumme) {
+            setVersicherungsStatus("Sie sind unterversichert.")
+        } 
     }
 
     function handleClear() {
@@ -23,6 +34,8 @@ export default function Calculator() {
         setVersicherungswert("")
         setSchadenssumme("")
         setSchadensdeckung("")
+        setVersicherungsStatus("")
+        setVersicherungsText("")
     }
 
     return (
@@ -40,13 +53,13 @@ export default function Calculator() {
                         
                         <div className="inputBoxes">
                         <tr>
-                            <td><input className="input" type="number" value={versicherungssumme} placeholder="Versicherungssumme" onChange={ (e) => setVersicherungssumme(e.target.value) }/></td>
+                            <td><input id="versicherungssumme" className="input" type="number" value={versicherungssumme} placeholder="Versicherungssumme" onChange={ (e) => setVersicherungssumme(e.target.value) }/></td>
                         </tr>
                         <tr>
-                            <td><input className="input" type="number" value={versicherungswert} placeholder="Versicherungswert" onChange={ (e) => setVersicherungswert(e.target.value) }/></td>
+                            <td><input id="versicherungswert" className="input" type="number" value={versicherungswert} placeholder="Versicherungswert" onChange={ (e) => setVersicherungswert(e.target.value) }/></td>
                         </tr>
                         <tr>
-                            <td><input className="input" type="number" value={schadenssumme} placeholder="Schadenssumme" onChange={ (e) => setSchadenssumme(e.target.value) }/></td>
+                            <td><input id="schadenssumme" className="input" type="number" value={schadenssumme} placeholder="Schadenssumme" onChange={ (e) => setSchadenssumme(e.target.value) }/></td>
                         </tr>
                         </div>
 
@@ -61,9 +74,12 @@ export default function Calculator() {
                 </table>
             </form>
             </div>
+            
+            <h2> {versicherungsText} </h2>
 
-            <h3>Schadensdeckung der Hausratsversicherung: </h3>
-            <h3> CHF {schadensdeckung}</h3>
+            <h2> {schadensdeckung} </h2>
+
+            <h2> {versicherungsStatus} </h2>
 
         </div>
 
